75a756e7fa8272a085ccf2861d4872d56c369805
pratiksha_chaure <pratikshachaure381@gmail.com>
2024-07-14 19:33:19 +0530

    hii

--- 4_multiplication.py
+++ 4_multiplication.py
@@ -1,4 +1,4 @@
 number1 = int(input("Enter an integer: "))
-number2 = int (input ("Enter second interger Number :- "))
+number2 = int (input ("Enter second interger Number :- andfdfjsdlf"))
 multiplication=number1*number2
 print("Multiplication  = ",multiplication)
