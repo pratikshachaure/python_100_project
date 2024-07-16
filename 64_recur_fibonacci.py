1950b09aa8b6c97edf1d3973b735316835ed0429
pratiksha_chaure <pratikshachaure381@gmail.com>
2024-07-16 16:48:43 +0530

    recur

--- 64_recur_fibonacci.py
+++ 64_recur_fibonacci.py
@@ -1,1 +1,8 @@
- 
+def sum_fibonacci(n, first=0, second=1):
+    if n > 0:
+        print(first,"   ")
+        third = first + second
+        first = second
+        second = third
+        sum_fibonacci(n-1, first, second) 
+sum_fibonacci(10)
